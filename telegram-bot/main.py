1cda390a9cb8d58282c4201b7557c9021e09bd64
Dragonprod <artyom2002-98@mail.ru>
2021-11-04 13:28:23 +0300

    add bot template

---
+++ telegram-bot/main.py
@@ -0,0 +1,8 @@
+import logging
+from src.app import Bot
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+if __name__ == '__main__':  
+    bot = Bot()
+    bot.run()
---
+++ telegram-bot/src/core/config.py
@@ -0,0 +1,4 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+ADMINS_CHAT_IDS = os.getenv('ADMINS_CHAT_IDS').split(' ')
