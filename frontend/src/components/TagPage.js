8ea954a3ecbd82366a1f225fcda2778d447daebe
Dragonprod <artyom2002-98@mail.ru>
2021-11-10 23:53:53 +0300

    add loader, add table

--- frontend/src/components/TagPage.js
+++ frontend/src/components/TagPage.js
@@ -1,4 +1,13 @@
 /* eslint-disable no-unused-vars */
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
 import React, { PureComponent } from "react";
 import styles from "../styles//ReferenceBookPage.module.css";
 
@@ -7,11 +16,31 @@ class TagPage extends PureComponent {
     const { name, description, attrs } = this.props;
 
     let desc = description.length > 0 ? description : "Описание отсутствует";
-
+    
     return (
       <div className={styles.textContainer}>
         <h2 className={styles.themeTitle}>{name}</h2>
         <p className={styles.text}>{desc}</p>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Свойство</TableCell>
+                <TableCell align="left">Описание</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {attrs.map((attr) => (
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="left">{attr.attributeName}</TableCell>
+                  <TableCell align="left">{attr.description}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
--- frontend/src/pages/ReferenceBookPage.js
+++ frontend/src/pages/ReferenceBookPage.js
@@ -9,6 +9,7 @@ import ListItemText from "@mui/material/ListItemText";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
+import CircularProgress from "@mui/material/CircularProgress";
 import API from "../api/api";
 import { renderTagsList } from "../services/renders";
 import TagPage from "../components/TagPage";
@@ -22,11 +23,12 @@ export const tagsAndProperties = [
 
 export default function ReferenceBook() {
   const [tags, setTags] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getTagsApi = async () => {
       const response = await API.get("/tag");
       setTags(response.data);
+      setLoading(false);
     };
     getTagsApi();
   }, []);
@@ -35,45 +37,53 @@ export default function ReferenceBook() {
   return (
     <>
       <ReferenceBookMenu />
-      <div className={styles.mainGrid}>
-        <div className={styles.sidebar}>
-          <Stack spacing={2} sx={{ width: 240 }}>
-            <Autocomplete
-              className={styles.searchbar}
-              id="free-solo-demo"
-              freeSolo
-              options={tagsAndProperties.map((option) => option.title)}
-              renderInput={(params) => <TextField {...params} label="Поиск" />}
-            />
-          </Stack>
-          <h2 className={styles.htmlTitle}>HTML</h2>
-          <nav
-            className={styles.sidebarNavLinks}
-            aria-label="secondary mailbox folders"
-          >
-            <List>{tagsData}</List>
-          </nav>
-          <h2 className={styles.cssTitle}>CSS</h2>
-          <nav
-            className={styles.sidebarNavLinks}
-            aria-label="secondary mailbox folders"
-          >
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#align-content">
-                  <ListItemText primary="align-content" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#align-items">
-                  <ListItemText primary="align-items" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </nav>
-        </div>
+      {loading && (
         <div className={styles.mainСontent}>
-          {/* <div className={styles.textContainer}>
+          <CircularProgress />
+        </div>
+      )}
+      {!loading && (
+        <div className={styles.mainGrid}>
+          <div className={styles.sidebar}>
+            <Stack spacing={2} sx={{ width: 240 }}>
+              <Autocomplete
+                className={styles.searchbar}
+                id="free-solo-demo"
+                freeSolo
+                options={tagsAndProperties.map((option) => option.title)}
+                renderInput={(params) => (
+                  <TextField {...params} label="Поиск" />
+                )}
+              />
+            </Stack>
+            <h2 className={styles.htmlTitle}>HTML</h2>
+            <nav
+              className={styles.sidebarNavLinks}
+              aria-label="secondary mailbox folders"
+            >
+              <List>{tagsData}</List>
+            </nav>
+            <h2 className={styles.cssTitle}>CSS</h2>
+            <nav
+              className={styles.sidebarNavLinks}
+              aria-label="secondary mailbox folders"
+            >
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton component="a" href="#align-content">
+                    <ListItemText primary="align-content" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component="a" href="#align-items">
+                    <ListItemText primary="align-items" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </nav>
+          </div>
+          <div className={styles.mainСontent}>
+            {/* <div className={styles.textContainer}>
             <h2 className={styles.themeTitle}>Быстрый старт</h2>
             <p className={styles.text}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
@@ -82,9 +92,14 @@ export default function ReferenceBook() {
             </p>
           </div>
           <div className={styles.codeContainer}></div> */}
-          <TagPage name = "Test TEST tEsT" description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum" attrs = "" />
+            <TagPage
+              name={tags[2].name}
+              description={tags[2].description}
+              attrs={tags[2].attributes}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
