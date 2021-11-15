aac05ac687c578cf801da0d13c569449cf9c1695
Shmyaks <maxonic10@list.ru>
2021-11-15 16:43:38 +0300

    Skeleton Animation added

--- frontend/src/pages/ReferenceBookPage.js
+++ frontend/src/pages/ReferenceBookPage.js
@@ -4,7 +4,6 @@ import Menu from "../components/ReferenceMenu";
 import styles from "../styles//ReferenceBookPage.module.css";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
@@ -12,8 +11,10 @@ import Autocomplete from "@mui/material/Autocomplete";
 import CircularProgress from "@mui/material/CircularProgress";
 import API from "../api/api";
 import TagPage from "../components/TagPage";
+import CodeEditor from "../components/CodeEditor";
+import Skeleton from '@mui/material/Skeleton';
 
-export default function ReferenceBook() {
+const ReferenceBook = () => {
   const [tags, setTags] = useState([]);
   const [loading, setLoading] = useState(true);
   const [tagPage, settagPage] = useState();
@@ -23,11 +24,11 @@ export default function ReferenceBook() {
       const response = await API.get("/tag");
       setTags(response.data);
       settagPage(
-        <TagPage
-          name={response.data[0].name}
-          description={response.data[0].description}
-          attrs={response.data[0].attributes}
-        />
+          <TagPage
+              name={response.data[0].name}
+              description={response.data[0].description}
+              attrs={response.data[0].attributes}
+          />
       );
       setLoading(false);
     };
@@ -36,65 +37,96 @@ export default function ReferenceBook() {
 
   const handleItemChange = (tag) => {
     settagPage(
-      <TagPage
-        name={tag.name}
-        description={tag.description}
-        attrs={tag.attributes}
-      />
+        <TagPage
+            name={tag.name}
+            description={tag.description}
+            attrs={tag.attributes}
+        />
     );
   };
 
   const tagsData = tags.map((tag) => (
-    <ListItem
-      key={tag.id}
-      onClick={() => {
-        handleItemChange(tag);
-      }}
-    >
-      <ListItemText primary={tag.name} />
-    </ListItem>
+      <ListItem
+          key={tag.id}
+          onClick={() => {
+            handleItemChange(tag);
+          }}
+      >
+        <ListItemText primary={tag.name} />
+      </ListItem>
   ));
 
   return (
-    <>
-      <Menu />
-      {loading && (
-        <div className={styles.mainСontent}>
-          <CircularProgress />
-        </div>
-      )}
-      {!loading && (
-        <div className={styles.mainGrid}>
-          <div className={styles.sidebar}>
-            <Stack spacing={2} sx={{ width: 240 }}>
-              <Autocomplete
-                className={styles.searchbar}
-                id="search-main"
-                freeSolo
-                options={tags.map((option) => option.name)}
-                renderInput={(params) => (
-                  <TextField {...params} label="Поиск" />
-                )}
-              />
-            </Stack>
-            <h2 className={styles.htmlTitle}>HTML</h2>
-            <nav className={styles.sidebarNavLinks} aria-label="html tags">
-              <React.Fragment>
-                <List>{tagsData}</List>
-              </React.Fragment>
-            </nav>
-            <h2 className={styles.cssTitle}>CSS</h2>
-            <nav className={styles.sidebarNavLinks} aria-label="css tags">
-              <List>
-                <ListItem>
-                  <ListItemText primary="css-tag" />
-                </ListItem>
-              </List>
-            </nav>
-          </div>
-          <div className={styles.mainСontent}>{tagPage}</div>
-        </div>
-      )}
-    </>
+      <>
+        <Menu />
+        {loading && (
+            <>
+              {/* <CircularProgress /> */}
+              <div className={styles.mainGrid}>
+                <div className={styles.sidebar}>
+                  <Skeleton variant="rectangular" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '20px' }} />
+                  <Skeleton variant="text" height={60} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '10px', width: '70%' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+
+                  <Skeleton variant="text" height={60} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '10px', width: '70%' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+                </div>
+                <div className={styles.mainСontent}>
+                  <Skeleton variant="text" width={300} height={70} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '-13px' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
+                  <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
+                  <Skeleton variant="rectangular" height={500} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '20px', marginTop: '40px' }} />
+                </div>
+              </div>
+            </>
+        )}
+        {!loading && (
+            <div className={styles.mainGrid}>
+              <div className={styles.sidebar}>
+                <Stack spacing={2} sx={{ width: 240 }}>
+                  <Autocomplete
+                      className={styles.searchbar}
+                      id="search-main"
+                      freeSolo
+                      options={tags.map((option) => option.name)}
+                      renderInput={(params) => (
+                          <TextField {...params} label="Поиск" />
+                      )}
+                  />
+                </Stack>
+                <h2 className={styles.htmlTitle}>HTML</h2>
+                <nav className={styles.sidebarNavLinks} aria-label="html tags">
+                  <React.Fragment>
+                    <List>{tagsData}</List>
+                  </React.Fragment>
+                </nav>
+                <h2 className={styles.cssTitle}>CSS</h2>
+                <nav className={styles.sidebarNavLinks} aria-label="css tags">
+                  <List>
+                    <ListItem>
+                      <ListItemText primary="css-tag" />
+                    </ListItem>
+                  </List>
+                </nav>
+              </div>
+              <div className={styles.mainСontent}>
+                {tagPage}
+                {/* <CodeEditor /> */}
+              </div>
+            </div>
+        )}
+      </>
   );
-}
+};
+export default ReferenceBook;
