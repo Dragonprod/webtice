bd03e98de83a589ee4ac9ed1a5eeb05490a97dab
Dragonprod <artyom2002-98@mail.ru>
2021-11-18 23:07:59 +0300

    css tags render

--- frontend/src/pages/ReferenceBookPage.js
+++ frontend/src/pages/ReferenceBookPage.js
@@ -8,32 +8,51 @@ import ListItemText from "@mui/material/ListItemText";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-import CircularProgress from "@mui/material/CircularProgress";
 import API from "../api/api";
 import TagPage from "../components/TagPage";
 import CodeEditor from "../components/CodeEditor";
-import Skeleton from '@mui/material/Skeleton';
+import Skeleton from "@mui/material/Skeleton";
+
+
+import ListItemButton from '@mui/material/ListItemButton';
+
+import Collapse from '@mui/material/Collapse';
+
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
 
 const ReferenceBook = () => {
-  const [tags, setTags] = useState([]);
+  const [htmlTags, sethtmlTags] = useState([]);
+  const [cssTags, setcssTags] = useState([]);
   const [loading, setLoading] = useState(true);
   const [tagPage, settagPage] = useState();
+  //const [open, setOpen] = useState(true);
 
   useEffect(() => {
-    const getTagsApi = async () => {
-      const response = await API.get("/tag");
-      setTags(response.data);
+    const getTagsData = async () => {
+      const htmlResponse = await API.get("/tag");
+      sethtmlTags(htmlResponse.data);
       settagPage(
         <TagPage
-          name={response.data[0].name}
-          description={response.data[0].description}
-          attrs={response.data[0].attributes}
+          name={htmlResponse.data[0].name}
+          description={htmlResponse.data[0].description}
+          attrs={htmlResponse.data[0].attributes}
         />
       );
+
+      const cssResponse = await API.get("/style");
+      setcssTags(cssResponse.data);
+
       setLoading(false);
     };
-    getTagsApi();
-  }, []);
+
+    getTagsData();
+  }, []); 
+
+  // const handleClick = () => {
+  //   setOpen(!open);
+  // };
 
   const handleItemChange = (tag) => {
     settagPage(
@@ -45,14 +64,25 @@ const ReferenceBook = () => {
     );
   };
 
-  const tagsData = tags.map((tag) => (
+  const htmlTagsData = htmlTags.map((htmlTag) => (
     <ListItem
-      key={tag.id}
+      key={htmlTag.id}
       onClick={() => {
-        handleItemChange(tag);
+        handleItemChange(htmlTag);
       }}
     >
-      <ListItemText primary={tag.name} />
+      <ListItemText primary={htmlTag.name} />
+    </ListItem>
+  ));
+
+  const cssTagsData = cssTags.map((cssTag) => (
+    <ListItem
+      key={cssTag.id}
+      // onClick={() => {
+      //   handleItemChange(cssTag);
+      // }}
+    >
+      <ListItemText primary={cssTag.styleName} />
     </ListItem>
   ));
 
@@ -61,32 +91,190 @@ const ReferenceBook = () => {
       <Menu />
       {loading && (
         <>
-          {/* <CircularProgress /> */}
           <div className={styles.mainGrid}>
             <div className={styles.sidebar}>
-              <Skeleton variant="rectangular" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '20px' }} />
-              <Skeleton variant="text" height={60} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '10px', width: '70%' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+              <Skeleton
+                variant="rectangular"
+                height={40}
+                sx={{ bgcolor: "rgba(255,255,255,.1)", borderRadius: "20px" }}
+              />
+              <Skeleton
+                variant="text"
+                height={60}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  marginTop: "10px",
+                  width: "70%",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
 
-              <Skeleton variant="text" height={60} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '10px', width: '70%' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', margin: '-4px 0 0 30px' }} />
+              <Skeleton
+                variant="text"
+                height={60}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  marginTop: "10px",
+                  width: "70%",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  margin: "-4px 0 0 30px",
+                }}
+              />
             </div>
             <div className={styles.mainСontent}>
-              <Skeleton variant="text" width={300} height={70} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '10px', marginTop: '-13px' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
-              <Skeleton variant="text" height={40} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '5px', margin: '-8px 0' }} />
-              <Skeleton variant="rectangular" height={500} sx={{ bgcolor: 'rgba(255,255,255,.1)', borderRadius: '20px', marginTop: '40px' }} />
+              <Skeleton
+                variant="text"
+                width={300}
+                height={70}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "10px",
+                  marginTop: "-13px",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "5px",
+                  margin: "-8px 0",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "5px",
+                  margin: "-8px 0",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "5px",
+                  margin: "-8px 0",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "5px",
+                  margin: "-8px 0",
+                }}
+              />
+              <Skeleton
+                variant="text"
+                height={40}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "5px",
+                  margin: "-8px 0",
+                }}
+              />
+              <Skeleton
+                variant="rectangular"
+                height={500}
+                sx={{
+                  bgcolor: "rgba(255,255,255,.1)",
+                  borderRadius: "20px",
+                  marginTop: "40px",
+                }}
+              />
             </div>
           </div>
         </>
@@ -99,25 +287,34 @@ const ReferenceBook = () => {
                 className={styles.searchbar}
                 id="search-main"
                 freeSolo
-                options={tags.map((option) => option.name)}
+                options={htmlTags.map((option) => option.name)}
                 renderInput={(params) => (
                   <TextField {...params} label="Поиск" />
                 )}
               />
             </Stack>
+            {/* <ListItemButton onClick={handleClick}>
+              <ListItemText primary="HTML" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <React.Fragment>
+                  <List>{htmlTagsData}</List>
+                </React.Fragment>
+              </List>
+            </Collapse> */}
             <h2 className={styles.htmlTitle}>HTML</h2>
             <nav className={styles.sidebarNavLinks} aria-label="html tags">
               <React.Fragment>
-                <List>{tagsData}</List>
+                <List>{htmlTagsData}</List>
               </React.Fragment>
             </nav>
             <h2 className={styles.cssTitle}>CSS</h2>
             <nav className={styles.sidebarNavLinks} aria-label="css tags">
-              <List>
-                <ListItem>
-                  <ListItemText primary="css-tag" />
-                </ListItem>
-              </List>
+              <React.Fragment>
+                <List>{cssTagsData}</List>
+              </React.Fragment>
             </nav>
           </div>
           <div className={styles.mainСontent}>
