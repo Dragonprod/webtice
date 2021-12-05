4c29a5d41dcc461a98fafe9ba084c3a0eb04ee1c
maxcore25 <mkorobov2002@mail.ru>
2021-12-05 20:16:27 +0300

    CSS Code Card stylized

--- frontend/src/components/CssPage.js
+++ frontend/src/components/CssPage.js
@@ -7,39 +7,42 @@ import {
   TableCell,
   TableBody,
   Paper,
-} from "@mui/material";
-import React, { PureComponent } from "react";
-import styles from "../styles//ReferenceBookPage.module.css";
+} from '@mui/material';
+import React, { PureComponent } from 'react';
+import styles from '../styles//ReferenceBookPage.module.css';
 
 class CssPage extends PureComponent {
   render() {
     const { name, description, syntax, values } = this.props;
     const isValues = values.length > 0 ? true : false;
 
-    let desc = description.length > 0 ? description : "Описание отсутствует";
+    let desc = description.length > 0 ? description : 'Описание отсутствует';
 
     return (
       <React.Fragment>
         <div className={styles.textContainer}>
           <h2 className={styles.themeTitle}>{name}</h2>
           <p className={styles.descriptionText}>{desc}</p>
-          <code>{syntax}</code>
+          <code className={`${syntax.length > 0 ? styles.codeCard : ''}`}>
+            {syntax}
+          </code>
           {isValues && (
             <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="properties table">
+              <Table sx={{ minWidth: 650 }} aria-label='properties table'>
                 <TableHead>
                   <TableRow>
-                    <TableCell align="left">Свойство</TableCell>
-                    <TableCell align="left">Описание</TableCell>
+                    <TableCell align='left'>Свойство</TableCell>
+                    <TableCell align='left'>Описание</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {values.map((value) => (
+                  {values.map(value => (
                     <TableRow
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell align="left">{value.valueName}</TableCell>
-                      <TableCell align="left">{value.description}</TableCell>
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}>
+                      <TableCell align='left'>{value.valueName}</TableCell>
+                      <TableCell align='left'>{value.description}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
