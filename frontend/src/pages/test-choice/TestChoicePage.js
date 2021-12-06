a85abb49665636c812fba23587bc537fd795504c
maxcore25 <mkorobov2002@mail.ru>
2021-12-07 01:38:41 +0300

    TestChoicePage stylized

--- frontend/src/pages/test-choice/TestChoicePage.js
+++ frontend/src/pages/test-choice/TestChoicePage.js
@@ -2,12 +2,29 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import styles from './TestChoicePage.module.css';
+import Menu from '../../components/referencemenu/ReferenceMenu';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const TestChoicePage = () => {
   return (
-    <Typography variant='h1' component='div' gutterBottom align='center'>
-      Tests
-    </Typography>
+    <div className={styles.mainGrid}>
+      <Menu />
+      <h2 className={styles.testChoiceTitle}>Выберите тему теста</h2>
+      <div className={styles.testsGrid}>
+        <div className={styles.testThemeCard}>
+          <h3 className={styles.testTitle}>Тема 1</h3>
+          <ArrowForwardIosIcon className={styles.nextIcon} />
+        </div>
+        <div className={styles.testThemeCard}>
+          <h3 className={styles.testTitle}>Тема 2</h3>
+          <ArrowForwardIosIcon className={styles.nextIcon} />
+        </div>
+        <div className={styles.testThemeCard}>
+          <h3 className={styles.testTitle}>Тема 3</h3>
+          <ArrowForwardIosIcon className={styles.nextIcon} />
+        </div>
+      </div>
+    </div>
   );
 };
 export default TestChoicePage;
