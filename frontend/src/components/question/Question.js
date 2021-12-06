156e873487315a75a19d64102c4cd30025434729
Dragonprod <artyom2002-98@mail.ru>
2021-12-06 13:45:51 +0300

    add question

---
+++ frontend/src/components/question/Question.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+import styles from './Question.module.css';
+
+export default function Question(props) {
+    const theme, questionName, answers = props;
+
+    return (
+        <div>
+            <h1>{theme}</h1>
+            <h1>{questionName}</h1>
+            <FormControl component="fieldset">
+                <RadioGroup
+                    aria-label="gender"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                >
+                    {answers.map((ans) => (
+                        <FormControlLabel value={ans.answerName} control={<Radio />} label={ans.answerName} />
+                    ))}
+                </RadioGroup>
+            </FormControl>
+        </div>
+    );
+}
