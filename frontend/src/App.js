8f125e019b8dd7172470ee508b5a7d970f87e572
maxcore25 <mkorobov2002@mail.ru>
2021-12-07 00:33:52 +0300

    TestChoicePage created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LandingPage from "./pages/landing/LangingPage";
-import ReferenceBookPage from "./pages/referencebook/ReferenceBookPage";
-import ErrorPage from "./pages/system/ErrorPage";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './pages/landing/LangingPage';
+import ReferenceBookPage from './pages/referencebook/ReferenceBookPage';
+import ErrorPage from './pages/system/ErrorPage';
+import TestChoicePage from './pages/tests/TestChoicePage';
 
 function App() {
   // TODO: Add components for exam and learn path
   return (
-    <div className="App">
+    <div className='App'>
       <main>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/refbook" component={ReferenceBookPage} />
-            <Route path="/exam" component={LandingPage} />
-            <Route path="/learn" component={LandingPage} />
-            <Route path="*" component={ErrorPage}/>
+            <Route exact path='/' component={LandingPage} />
+            <Route path='/refbook' component={ReferenceBookPage} />
+            <Route path='/exam' component={LandingPage} />
+            <Route path='/learn' component={LandingPage} />
+            <Route path='/test-choice' component={TestChoicePage} />
+            <Route path='*' component={ErrorPage} />
           </Switch>
         </BrowserRouter>
       </main>
---
+++ frontend/src/pages/tests/TestChoicePage.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import styles from './TestChoicePage.module.css';
+
+const TestChoicePage = () => {
+  return (
+    <Typography variant='h1' component='div' gutterBottom align='center'>
+      Tests
+    </Typography>
+  );
+};
+export default TestChoicePage;
