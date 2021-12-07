55bd84685be9f68c84e4a8af8c27a1c9ddc1fffd
Dragonprod <artyom2002-98@mail.ru>
2021-12-07 07:48:57 +0300

    fix question

--- frontend/src/pages/test/TestPage.js
+++ frontend/src/pages/test/TestPage.js
@@ -6,6 +6,7 @@ import API from '../../api/api';
 import Question from '../../components/question/Question';
 
 const TestPage = () => {
+  const [current, setCurrent] = useState(0);
   const [questions, setQuestions] = useState([])
   const [loading, setLoading] = useState(true)
 
@@ -26,7 +27,21 @@ const TestPage = () => {
           <Menu />
           <h2 className={styles.testChoiceTitle}>Выберите тему теста</h2>
           <div className={styles.testsGrid}>
-            <Question theme="Тема 1" questionName={questions[0].questionName} answers={questions[0].answers} />
+            {
+              {
+                0: <Question theme="Тема 1" questionName={questions[0].questionName} answers={questions[0].answers} />,
+                1: <Question theme="Тема 1" questionName={questions[1].questionName} answers={questions[1].answers} />,
+                2: <Question theme="Тема 1" questionName={questions[2].questionName} answers={questions[2].answers} />,
+                3: <Question theme="Тема 1" questionName={questions[3].questionName} answers={questions[3].answers} />,
+                4: <Question theme="Тема 1" questionName={questions[4].questionName} answers={questions[4].answers} />,
+                5: <Question theme="Тема 1" questionName={questions[5].questionName} answers={questions[5].answers} />,
+                6: <Question theme="Тема 1" questionName={questions[6].questionName} answers={questions[6].answers} />,
+                7: <Question theme="Тема 1" questionName={questions[7].questionName} answers={questions[7].answers} />,
+                8: <Question theme="Тема 1" questionName={questions[8].questionName} answers={questions[8].answers} />,
+                9: <Question theme="Тема 1" questionName={questions[9].questionName} answers={questions[9].answers} />
+              }[current]
+            }
+
           </div>
         </div>
       )}
